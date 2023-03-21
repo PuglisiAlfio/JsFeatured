6e410929fcd43189f41e67e69b55c840e3e4653c
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-21 11:11:01 +0100

    esercizio 25 con spread operator

---
+++ es-25/exercise-25.js
@@ -0,0 +1,7 @@
+let numberStore = [0, 1, 2];
+
+let newNumberStore = [...numberStore, 3]
+// let newNumber = 3;
+// numberStore.push(newNumber);
+console.log(numberStore);
+console.log(newNumberStore);
