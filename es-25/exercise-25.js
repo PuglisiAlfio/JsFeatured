29d62de04faf1d550ee2861f436e86beca08559e
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-21 11:21:44 +0100

    Esercizio 25 con spread

--- es-25/exercise-25.js
+++ es-25/exercise-25.js
@@ -1,7 +1,7 @@
 let numberStore = [0, 1, 2];
 
-let newNumberStore = [...numberStore, 3]
-// let newNumber = 3;
+let newNumber = 3;
+let newNumberStore = [...numberStore, newNumber]
 // numberStore.push(newNumber);
 console.log(numberStore);
 console.log(newNumberStore);
