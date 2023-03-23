fa8c93f087c644d32ad7b0e264313676e9011e64
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-23 10:52:20 +0100

    Esercizio 28 con spread Operator

--- es-28/exercise-28.js
+++ es-28/exercise-28.js
@@ -1,9 +1,8 @@
-function sum(...items) {
-  let somma = 0;
-  for (let item of items)
-  somma += item
-  return somma;
+function sum(num1, num2, num3) {
+  return num1 + num2 + num3;
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+
+const numbers = [1, 2, 3];
 console.log(sum(...numbers));
