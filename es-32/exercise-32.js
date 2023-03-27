249580ded618630c3bc268319ead28cce079384a
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-27 12:07:50 +0200

    Esercizio 32 svolto con rest operator

---
+++ es-32/exercise-32.js
@@ -0,0 +1,10 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+};
+
+const {firstName, lastName, ...rest} = person;
+
+console.log(JSON.stringify(rest)); // Should return: { "id": 1, "age": 25 }
